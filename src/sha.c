fa0d14f69af863ee2a9b91aef166dc0b7d8a450c
Rıdvan Korkmaz <155473546+ridvankorkmaz@users.noreply.github.com>
2024-02-08 23:08:10 +0300

    Update sha.c

--- src/sha.c
+++ src/sha.c
@@ -11,7 +11,17 @@
  *
  * Portions Copyright (c) 2001-2009, PostgreSQL Global Development Group
  */
+
+// pg16-compability
 #include "postgres.h"
+#ifndef SET_VARSIZE
+/*
+ * pg >= 16 reorganized the toastable header files
+ * https://github.com/postgres/postgres/commit/d952373a987bad331c0e499463159dd142ced1ef
+ * to ensure cross version compatibility we do a bit of a hack here
+ */
+#include "varatt.h"
+#endif
 
 #include "access/hash.h"
 #include "common.h"
